c3bff64125f0e523e3ac45cedf95704b71b95b66
Abdullah Nasir <abdullahnasirab23@gmail.com>
2025-02-26 19:47:34 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -149,7 +149,7 @@ if(bodyid === "home"){
     let proj = document.querySelector('.proj-contain');
   function btn1(){
           let pro_img =  document.querySelector('#pro-img');
-    pro_img.src = '/img/capture1.png';
+    pro_img.src='../img/capture1.png';
   
     proj_name.innerHTML = 'Design & Develop Responsive Agriculture Site';
     pro_img.classList.add('text-focus-in');
@@ -161,7 +161,7 @@ if(bodyid === "home"){
   }
   function btn2(){
           let pro_img =  document.querySelector('#pro-img');
-    pro_img.src = '/img/capture.png';
+    pro_img.src='../img/capture.png';
     proj_name.innerHTML = 'Design & Develop Responsive Luxury Restaurant Site';
     pro_img.classList.add('text-focus-in');
     proj_name.classList.add('text-focus-in');
